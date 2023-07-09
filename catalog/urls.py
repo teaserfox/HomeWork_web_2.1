7e0607a396b7e474f37a5dd5a99d6c57f3340ae5
Kokorina Tatyana <teaserfox@gmail.com>
2023-07-09 17:10:22 +0300

    'вопрос по post-запросу'

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+
+from catalog.views import contacts, home
+
+urlpatterns = [
+    path('', contacts, name='contacts'),
+    path('', home, name='home')
+]
---
+++ catalog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print(f'Имя: {name}\n Телефон {phone}\n Сообщение {message}')
+
+    return render(request, 'catalog/contacts.html')
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
